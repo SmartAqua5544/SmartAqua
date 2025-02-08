76f32f5b0df5d223c2dd66cec81bd96f2f3402e0
SmartAqua5544 <smartaquaoficial@gmail.com>
2025-02-08 15:27:53 -0300

    index.js

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post('/sensors', (req, res) => {
 });
 
 /* App listen */
-app.listen(3000, () => {
+app.listen(3000, '0.0.0.0', () => {
   console.log(`nodejs-backend is running`);
   console.log(`open in http://54.87.37.37:3000/sensors`);
 });
